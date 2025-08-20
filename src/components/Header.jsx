37bc3c2c692b3d41fcdd9251f25d1c73d450ac71
Juan Frois <juan.frois9@gmail.com>
2025-08-20 20:55:11 -0300

    corrigir: atualizar a dependência react_jsx-runtime e corrigir erro de digitação no componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ function Header() {
                         <a href="/consumidor.html">Sou consumidor</a>
                     </li>
                     <li className="nav-item">
-                        <a href="/login.html">Ja possuo contato</a>
+                        <a href="/login.html">Ja possuo conta</a>
                     </li>
                     <li className="nav-item">
                         <a href="/contato.html">Entre em contato</a>
