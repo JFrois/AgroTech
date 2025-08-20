dea498fd10a3b168664e69b9c80880ad93e9e1f4
Juan Frois <112560665+JFrois@users.noreply.github.com>
2025-08-20 11:57:07 -0300

    Comentando toda a etapa de teste.

--- src/javascript/componentes.js
+++ src/javascript/componentes.js
@@ -1,27 +1,24 @@
-// src/javascript/meu-componente.js
+// 'use strict';
 
-// O 'use strict' é uma boa prática
-'use strict';
+// // Exemplo de um componente simples: um botão de "curtir"
+// const LikeButton = () => {
+//     const [liked, setLiked] = React.useState(false);
 
-// Exemplo de um componente simples: um botão de "curtir"
-const LikeButton = () => {
-    const [liked, setLiked] = React.useState(false);
+//     if (liked) {
+//         return 'Você curtiu isso!';
+//     }
 
-    if (liked) {
-        return 'Você curtiu isso!';
-    }
+//     // A sintaxe <button>...</button> é o JSX. O Babel vai transformá-la.
+//     return (
+//         <button onClick={() => setLiked(true)}>
+//             Curtir 👍
+//         </button>
+//     );
+// }
 
-    // A sintaxe <button>...</button> é o JSX. O Babel vai transformá-la.
-    return (
-        <button onClick={() => setLiked(true)}>
-            Curtir 👍
-        </button>
-    );
-}
-
-// Encontra a div 'react-root' no seu HTML
-const domContainer = document.querySelector('#react-root');
-// Cria a raiz de renderização do React
-const root = ReactDOM.createRoot(domContainer);
-// Renderiza o componente <LikeButton /> dentro da raiz
-root.render(<LikeButton />);
+// // Encontra a div 'react-root' no seu HTML
+// const domContainer = document.querySelector('#react-root');
+// // Cria a raiz de renderização do React
+// const root = ReactDOM.createRoot(domContainer);
+// // Renderiza o componente <LikeButton /> dentro da raiz
+// root.render(<LikeButton />);
